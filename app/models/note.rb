7bdd8ab34df8cbb6933e69d583c597fbf099bdd9
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-12-16 22:15:30 +0000

    Add a Title fallback for failed meta fetch

--- app/models/note.rb
+++ app/models/note.rb
@@ -24,12 +24,11 @@ class Note < ApplicationRecord
       page = MetaInspector.new(link)
       self.title = page.best_title
       self.comment = page.description if self.comment.blank?
-      if self.tags.empty?
-        meta_tags = page.meta_tag.dig("name", "keywords").to_s.split(",").map(&:strip).reject { |t| t.scan(/[0-9]/).size > 2 }
-        title_tags = (self.title.scan(/[a-z]\S*[a-z]/i).map(&:downcase) - stop_words).join(",")
-        self.tag_list = [meta_tags, title_tags].join(",")
-      end
+      set_tags(page) if self.tags.empty?
     end
+
+  rescue
+    safe_default
   end
 
   def should_promote_link_to_title?
@@ -39,6 +38,17 @@ class Note < ApplicationRecord
   def promote_link_to_title
     self.title = self.link
     self.link = nil
+  end
+
+  def safe_default
+    self.link = '' if link.blank?
+    self.title = link || '' if title.blank?
+  end
+
+  def set_tags(page)
+    meta_tags = page.meta_tag.dig("name", "keywords").to_s.split(",").map(&:strip).reject { |t| t.scan(/[0-9]/).size > 2 }
+    title_tags = (self.title.scan(/[a-z]\S*[a-z]/i).map(&:downcase) - stop_words).join(",")
+    self.tag_list = [meta_tags, title_tags].join(",")
   end
 
   def stop_words
